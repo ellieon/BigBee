4b622c118380e40504bf7b4ddd7e4c5d57cbcd79
Ellie Harrison <ellie@vertigomoon.co.uk>
2020-06-13 17:07:25 +0100

    Rename localhost in tests

--- src/test/web/routes/index.spec.ts
+++ src/test/web/routes/index.spec.ts
@@ -1,17 +1,18 @@
 import { expect } from 'chai'
 import * as request from 'supertest'
+import { server } from 'web/index'
 
 describe('Index page', async () => {
   describe('On GET', async () => {
     it('should redirect to HTTPS when HTTP is attempted to be accessed', async () => {
-      const data = await request('http://localhost')
+      const data = await request('http://127.0.0.1')
         .get('/')
         .expect(302)
       expect(data.header.location).to.contain('https://')
     })
 
     it('should not redirect when HTTPS is accessed', async () => {
-      await request('https://localhost')
+      await request(server)
         .get('/')
         .expect(200)
     })
